c9240539a0ffad6fa46b921e93072c85f5bdc31f
Minh Chánh <158822707+minhchanhtrang@users.noreply.github.com>
2025-07-20 18:52:47 +0700

    Delete dothi_vohuong directory

--- dothi_vohuong/DSC_adjacent.c
+++
@@ -1,7 +0,0 @@
-int adjacent(Graph *pG, int u, int v){
-    int e;
-    for(e=0; e<pG->m; e++)
-        if((pG->edges[e].u==u && pG->edges[e].v==v) || (pG->edges[e].u==v && pG->edges[e].v==u))
-            return 1;
-    return 0;
-}
