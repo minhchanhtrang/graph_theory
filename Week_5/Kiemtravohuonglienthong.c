3aa7feb92a76d0c4d9cb4ca7069839554e04d931
Minh Chánh <158822707+minhchanhtrang@users.noreply.github.com>
2025-08-05 22:57:15 +0700

    Create Kiemtravohuonglienthong.c

---
+++ Week_5/Kiemtravohuonglienthong.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+/* Khai báo CTDL Graph*/
+#define MAX_N 100
+typedef struct {
+    int n, m;
+    int A[MAX_N][MAX_N];
+} Graph;
+
+void init_graph(Graph *pG, int n) {
+    pG->n = n;
+    pG->m = 0;
+    for (int u = 1 ; u <= n; u++)
+        for (int v = 1 ; v <= n; v++)
+            pG->A[u][v] = 0;
+}
+
+void add_edge(Graph *pG, int u, int v) {
+    pG->A[u][v] += 1;
+    if (u != v)
+        pG->A[v][u] += 1;
+    
+    pG->m++;
+}
+
+int adjacent(Graph *pG, int u, int v) {
+    return pG->A[u][v] > 0;
+}
+
+
+
+
+//Biến hỗ trợ dùng để lưu trạng thái của đỉnh: đã duyệt/chưa duyệt
+int mark[MAX_N];
+
+void DFS(Graph *pG, int u) {
+	//1. Đánh dấu u đã duyệt
+	//printf("Duyet %d\n", u);	//Làm gì đó trên u
+	mark[u] = 1; 			//Đánh dấu nó đã duyệt
+
+	//2. Xét các đỉnh kề của u
+	for (int v = 1; v <= pG->n; v++)
+		if (adjacent(pG, u, v) && mark[v] == 0) //Nếu v chưa duyệt
+			DFS(pG, v); 			    //gọi đệ quy duyệt nó	
+
+}
+
+// Kiểm tra pG có liên thông không
+int connected(Graph *pG) {
+	//1. Khởi tạo tất cả đỉnh đều chưa duyệt
+	for (int u = 1; u <= pG->n; u++)
+		mark[u] = 0;
+	//2. Duyệt đồ thị từ đỉnh bất kỳ, ví dụ: 1
+	DFS(pG, 1);
+	//3. Kiểm tra xem có đỉnh nào chưa duyệt không
+	for (int u = 1; u <= pG->n; u++)
+		if (mark[u] == 0)	//Vẫn còn đỉnh chưa duyệt
+			return 0; 	//Đồ thị không liên thông, thoát luôn
+
+	return 1;			//Tất cả các đỉnh đều đã duyệt => liên thông
+}
+
+
+int main() {
+	//1. Khai báo đồ thị G
+	Graph G;
+	//2. Đọc dữ liệu và dựng đồ thị
+	int n, m, u, v;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	for (int e = 0; e < m; e++) {
+	    scanf("%d%d", &u, &v);
+	    add_edge(&G, u, v);
+	}
+
+	//3. Khởi tạo mảng mark[u] = 0, với mọi u = 1, 2, ..., n
+	for (int u = 1; u <= G.n; u++) {
+		mark[u] = 0;
+	}
+
+	//4. Gọi hàm connected để kiểm tra
+
+    if (connected(&G))
+        printf("CONNECTED\n");
+    else
+        printf("DISCONNECTED\n");
+    
+    return 0;
+}
+
